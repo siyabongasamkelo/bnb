d115f60b41f3293dfb1c22f7b91d0ee8bedf1c1f
siyabonga <siyabongasamkelociam@gmail.com>
2024-12-19 19:47:53 +0200

    added theme provider

--- client/bnb/src/App.js
+++ client/bnb/src/App.js
@@ -6,8 +6,25 @@ import {
 } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const theme = {
+    color: {
+      primary: "#FFF7D1",
+      secondary: " #8B5DFF",
+      tetiary: "#6A42C2",
+      text: "rgba(0,0,0,0.7)",
+      smallText: "rgba(0, 0, 0, 0.6)",
+    },
+    size: {
+      small: "13px",
+      medium: "17px",
+      large: "22px",
+      extraLarge: "32px",
+    },
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
@@ -20,7 +37,9 @@ function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
