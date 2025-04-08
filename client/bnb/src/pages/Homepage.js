1f809623a775ad84397557b1038605ed85e84745
siyabonga <siyabongasamkelociam@gmail.com>
2025-04-08 15:25:24 +0200

    added aboutus and our room section to the home page

--- client/bnb/src/pages/Homepage.js
+++ client/bnb/src/pages/Homepage.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Home from "../components/Home";
+import AboutUs from "../components/AboutUs";
+import OurRooms from "../components/common/OurRooms";
 
 const Homepage = () => {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <AboutUs />
+      <OurRooms />
+    </>
+  );
 };
 
 export default Homepage;
